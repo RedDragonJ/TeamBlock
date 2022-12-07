2054bdc11fdd27cae0a182a95f275338482a9fc1
James Layton <jameshanxinsun@gmail.com>
2022-12-07 13:03:03 -0500

    Delete TeamBlock directory

--- TeamBlock/Pods/Target Support Files/Pods-TeamBlock-TeamBlockTests/Pods-TeamBlock-TeamBlockTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TeamBlock_TeamBlockTests : NSObject
-@end
-@implementation PodsDummy_Pods_TeamBlock_TeamBlockTests
-@end
--- TeamBlock/README.md
+++
@@ -1,4 +0,0 @@
-# Attributes
-### SwiftUI CachedAsyncImage (https://github.com/lorenzofiamingo/swiftui-cached-async-image)
-### SwiftLint (https://github.com/realm/SwiftLint)
-
